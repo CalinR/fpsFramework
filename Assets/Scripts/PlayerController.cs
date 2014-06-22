bedf802aed158f13e1d2328f0c9e22d457e24c81
Calin Reimer <calin.reimer@gmail.com>
2014-06-21 20:47:59 -0500

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+	private int activeWeapon = -1;
+
+	private int numOfWeaponsInInventory = 0;
+	public Weapon[] weaponList;
+	
+
+	void Awake()
+	{
+		Weapon weapon;
+		for(int i=0; i<weaponList.Length;i++)
+		{
+			weapon = weaponList[i];
+			weapon.gameObject.SetActive(false);
+			if(weapon.HasWeapon)
+			{
+				ActiveWeaponByIndex(i);
+			}
+		}
+
+	}
+
+	void Update()
+	{
+		Weapon weapon;
+		int weaponIndex;
+		//WEAPON HOTKEY
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			ActiveWeaponByIndex(0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			ActiveWeaponByIndex(1);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			ActiveWeaponByIndex(2);
+		}
+
+		//GOTO NEXT WEAPON IN WEAPON LIST
+		if (Input.GetKeyDown (KeyCode.E)) 
+		{
+			if(numOfWeaponsInInventory>0)
+			{	
+				weaponIndex = activeWeapon;
+				do 
+				{
+					weaponIndex++;
+					if(weaponIndex >= weaponList.Length)
+					{
+						weaponIndex = 0;
+					}
+
+					weapon = ActiveWeaponByIndex(weaponIndex);
+
+				}while(weapon==null);
+			}
+		}
+
+		//GOTO PREVIOUS WEAPON IN WEAPON LIST
+		if (Input.GetKeyDown (KeyCode.Q)) 
+		{
+			if(numOfWeaponsInInventory>0)
+			{	
+				weaponIndex = activeWeapon;
+				do 
+				{
+					weaponIndex--;
+					if(weaponIndex < 0)
+					{
+						weaponIndex = weaponList.Length-1;
+					}
+					
+					weapon = ActiveWeaponByIndex(weaponIndex);
+					
+				}while(weapon==null);
+			}
+		}
+	}
+
+
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Pickup"){
+			string weaponId = other.gameObject.name;
+			Weapon weapon;
+			for(int i = 0; i<weaponList.Length; i++)
+			{
+				weapon = weaponList[i];
+				if((weapon.id+"Pickup") == weaponId)
+				{
+					if(!weapon.HasWeapon)
+					{
+						numOfWeaponsInInventory++;
+						weapon.HasWeapon = true;
+						if(activeWeapon<0)
+						{
+							ActiveWeaponByIndex(i);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	Weapon ActiveWeaponByIndex(int index)
+	{
+		if (weaponList[index].HasWeapon) 
+		{
+			if(activeWeapon>=0){
+				weaponList[activeWeapon].gameObject.SetActive(false);
+			}
+			activeWeapon = index;
+			weaponList[index].gameObject.SetActive(true);
+
+			return weaponList[index];
+		} 
+		else 
+		{
+			return null;
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	[SerializeField]
+	private bool hasWeapon = false;
+	public string id;
+
+	public bool HasWeapon {
+		get 
+		{
+			return hasWeapon;
+		}
+		set
+		{ 
+			hasWeapon = value; 
+		}
+	}
+}
